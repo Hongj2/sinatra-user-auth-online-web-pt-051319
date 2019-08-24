b683ebaed32159e12ab1c394ceb60e8de6a7d9cb
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-08-24 16:40:45 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
   validates_presence_of :name, :email, :password
-   validates :name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
+  
 end
